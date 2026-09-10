1d38b6d61a56aec6ea3a9606bf445ced20c99769
zennmin <mouni56778@gmail.com>
2026-09-10 11:25:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -186,5 +186,8 @@ def decrypt(image_id):
     )
 
 
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
